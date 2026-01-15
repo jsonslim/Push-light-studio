d06e7cbb6e81f4defa4cbd386485a1e53ff74ec0
jsonslim <deluxe87d@gmail.com>
2026-01-15 01:33:57 +0100

    Fix TypeScript build errors for Railway deployment

--- src/context.tsx
+++ src/context.tsx
@@ -1,4 +1,4 @@
 // ColorContext.js
 import { createContext } from "react";
 
-export const ColorContext = createContext<number>(0);
+export const ColorContext = createContext<number | undefined>(undefined);
--- src/web-component.tsx
+++ src/web-component.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import reactToWebComponent from "react-to-webcomponent";
 import App from "./App";
 
 // Преобразуем React-компонент в Web Component
-const WebComp = reactToWebComponent(App, React, ReactDOM as any);
+const WebComp = reactToWebComponent(App, React, {} as any);
 
 // Регистрируем под тегом <my-app>
 customElements.define("PushLightStudio", WebComp);
